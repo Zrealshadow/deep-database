d87a2f6dc94d0a56f8c764c3a048d9ae3ec95980
working <57171759+NLGithubWP@users.noreply.github.com>
2025-11-22 21:57:33 +0800

    Add cache dir

--- tpberta/conver_needed_csv.py
+++ tpberta/conver_needed_csv.py
@@ -1,12 +1,12 @@
 from utils.data import DatabaseFactory
 
 db_name = "event"
+cache_dir = "/home/lingze/.cache/relbench/rel-event"
 db = DatabaseFactory.get_db(db_name,
+                            cache_dir=cache_dir,
                             upto_test_timestamp=False,
                             with_text_compress=True)
-dataset = DatabaseFactory.get_dataset(db_name)
-
-cache_dir = "/home/lingze/embedding_fusion/data/rel-event-tensor-frame"
+dataset = DatabaseFactory.get_dataset(db_name, cache_dir)
 
 task_name = "user-repeat"
 task = DatabaseFactory.get_task(db_name, task_name, dataset)
